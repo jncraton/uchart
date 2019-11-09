424edd4aad4d85c242684db89e2e25f096a7b951
Jon Craton <jncraton@gmail.com>
2019-11-09 09:22:28 -0500

    Add option for line chart

--- uchart.js
+++ uchart.js
@@ -1,4 +1,4 @@
-function uchart(canvas, y, margin=30) {
+function uchart(canvas, y, line=false, margin=30) {
   let ctx = canvas.getContext('2d')
   let min = y.reduce((min, yi) => yi < min ? yi : min, Infinity)
   let max = y.reduce((max, yi) => yi > max ? yi : max, -Infinity)
@@ -20,10 +20,21 @@ function uchart(canvas, y, margin=30) {
   ctx.scale(1,-1)
   ctx.translate(0,-canvas.height)
 
+  ctx.beginPath()
   y.forEach((yi, i) => {
     let x = margin + i * (canvas.width - 2*margin) / y.length
     let y_pos = margin + (yi - min) * (canvas.height - 2*margin) / (max-min)
-    ctx.fillRect(x,y_pos-3,6,6);
+
+    if (line) {
+      if (i == 0) {
+        ctx.moveTo(x,y_pos)
+      } else {
+        ctx.lineTo(x+3,y_pos)
+      }
+    }
+
+    ctx.fillRect(x,y_pos-3,6,6)
   })
+  ctx.stroke()
 
 }
