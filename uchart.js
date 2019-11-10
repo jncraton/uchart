301dca092321829a239d6ced322d6c0db07e90f1
Jon Craton <jncraton@gmail.com>
2019-11-10 09:44:54 -0500

    Accept a canvas context to allow more styling options

--- uchart.js
+++ uchart.js
@@ -1,14 +1,12 @@
-function uchart(canvas, y, line=false, margin=30) {
-  let ctx = canvas.getContext('2d')
+function uchart(ctx, y, line=false, margin=36) {
+  let canvas = ctx.canvas   
   let min = y.reduce((min, yi) => yi < min ? yi : min, Infinity)
   let max = y.reduce((max, yi) => yi > max ? yi : max, -Infinity)
+
   let y_labels = [max, (max+min)/2, min]
 
-  ctx.strokeStyle = '#ccc'
-  ctx.strokeWidth = .5
-
   ;[margin, canvas.height/2, canvas.height - margin].forEach((y, i) => {
-    ctx.fillText(y_labels[i], 5, y + 3)
+    ctx.fillText(y_labels[i], margin, y - 3)
     ctx.beginPath()
     ctx.moveTo(margin, y)
     ctx.lineTo(canvas.width - margin, y)
@@ -17,7 +15,7 @@ function uchart(canvas, y, line=false, margin=30) {
 
   ctx.beginPath()
   y.forEach((yi, i) => {
-    let x = margin + i * (canvas.width - 2*margin) / y.length
+    let x = margin*2 + i * (canvas.width - 3*margin) / y.length
     let y_pos = (margin + (yi - min) * (canvas.height - 2*margin) / (max-min))
     y_pos = canvas.height - y_pos
 
