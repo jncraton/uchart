e7884a4f010ebcdaf438ef422362de12241f5647
Jon Craton <jncraton@gmail.com>
2019-12-02 23:52:04 -0500

    Reduce number of moveTo and lineTo calls

--- uchart.js
+++ uchart.js
@@ -5,6 +5,11 @@ uchart = (ctx, y, line, margin=36) => {
   let min = Math.min(...y)
   let max = Math.max(...y)
 
+  let draw_line = (x1,y1,x2) => {
+    if (x2) ctx.moveTo(x2, y1)
+    if (x1) ctx.lineTo(x1, y1)  
+  }
+
   ctx.beginPath()
   ;[
     [margin,max], 
@@ -12,21 +17,21 @@ uchart = (ctx, y, line, margin=36) => {
     [height - margin, min]
   ].forEach(([y,label]) => {
     ctx.fillText(label, margin, y - 3)
-    ctx.moveTo(margin, y)
-    ctx.lineTo(width - margin, y)
+    draw_line(margin, y, width-margin)
   })
 
-  y.forEach((yi, i) => {
-    let x = margin*2 + i * (width - 3*margin) / y.length
+  y.reduce((prev, yi, i) => {
+    let x = margin*2 +  i * (width - 3*margin) / y.length
     let y_pos = margin + (yi - min) * (height - 2*margin) / (max-min)
     y_pos = height - y_pos
 
     if (line) {
-      if (i == 0) ctx.moveTo(x,y_pos)
-      else ctx.lineTo(x+3,y_pos)
+      draw_line(prev ? x : 0, y_pos, prev ? 0 : x+3 )
     }
 
     ctx.fillRect(x,y_pos-3,6,6)
-  })
+
+    return x
+  },0)
   ctx.stroke()
 }
