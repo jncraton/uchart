92d77512e03c7f918d69bb3ca850acbc03110634
Jon Craton <jncraton@gmail.com>
2019-12-02 22:37:24 -0500

    Cleaner access to width and height

--- uchart.js
+++ uchart.js
@@ -1,23 +1,24 @@
 function uchart(ctx, y, line=false, margin=36) {
-  let canvas = ctx.canvas   
+  let height = ctx.canvas.height
+  let width = ctx.canvas.width
   let min = Math.min(...y)
   let max = Math.max(...y)
 
   let y_labels = [max, (max+min)/2, min]
 
-  ;[margin, canvas.height/2, canvas.height - margin].forEach((y, i) => {
+  ;[margin, height/2, height - margin].forEach((y, i) => {
     ctx.fillText(y_labels[i], margin, y - 3)
     ctx.beginPath()
     ctx.moveTo(margin, y)
-    ctx.lineTo(canvas.width - margin, y)
+    ctx.lineTo(width - margin, y)
     ctx.stroke()
   })
 
   ctx.beginPath()
   y.forEach((yi, i) => {
-    let x = margin*2 + i * (canvas.width - 3*margin) / y.length
-    let y_pos = (margin + (yi - min) * (canvas.height - 2*margin) / (max-min))
-    y_pos = canvas.height - y_pos
+    let x = margin*2 + i * (width - 3*margin) / y.length
+    let y_pos = (margin + (yi - min) * (height - 2*margin) / (max-min))
+    y_pos = height - y_pos
 
     if (line) {
       if (i == 0) ctx.moveTo(x,y_pos)
